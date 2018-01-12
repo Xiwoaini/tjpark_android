6ccdbaaf743e7bd6baa9b553e66b39d38321b20d
futai <136260130@qq.com>
2018-01-12 14:46:42 +0800

    天津停车

---
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/PersonActivity.java
@@ -0,0 +1,16 @@
+package tjpark.tjsinfo.com.tjpark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by panning on 2018/1/12.
+ */
+
+public class PersonActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
+}
