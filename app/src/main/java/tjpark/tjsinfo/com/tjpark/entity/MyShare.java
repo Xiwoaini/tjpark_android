fa24debecbe99b75cb659b85f3540826bb6730da
futai <136260130@qq.com>
2018-01-16 09:26:25 +0800

    天津停车需要的基本类

---
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/entity/MyShare.java
@@ -0,0 +1,135 @@
+package tjpark.tjsinfo.com.tjpark.entity;
+
+/**
+ * Created by panning on 2018/1/15.
+ */
+
+public class MyShare {
+
+    private String id;
+    private String place_id;
+    private String place_name;
+    private String customer_id;
+    private String park_num;
+    private String phone;
+    private String create_time;
+    private String park_fee;
+    private String start_time;
+    private String end_time;
+    private String status;
+    private String share_status;
+    private String contacts_name;
+    private String buttonName;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPlace_id() {
+        return place_id;
+    }
+
+    public String getPlace_name() {
+        return place_name;
+    }
+
+    public String getCustomer_id() {
+        return customer_id;
+    }
+
+    public String getPark_num() {
+        return park_num;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public String getPark_fee() {
+        return park_fee;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getShare_status() {
+        return share_status;
+    }
+
+    public String getContacts_name() {
+        return contacts_name;
+    }
+
+    public String getButtonName() {
+        return buttonName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPlace_id(String place_id) {
+        this.place_id = place_id;
+    }
+
+    public void setPlace_name(String place_name) {
+        this.place_name = place_name;
+    }
+
+    public void setCustomer_id(String customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public void setPark_num(String park_num) {
+        this.park_num = park_num;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public void setPark_fee(String park_fee) {
+        this.park_fee = park_fee;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setShare_status(String share_status) {
+        this.share_status = share_status;
+    }
+
+    public void setContacts_name(String contacts_name) {
+        this.contacts_name = contacts_name;
+    }
+
+    public void setButtonName(String buttonName) {
+        this.buttonName = buttonName;
+    }
+}
---
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/entity/SharePark.java
@@ -0,0 +1,26 @@
+package tjpark.tjsinfo.com.tjpark.entity;
+
+/**
+ * Created by panning on 2018/1/15.
+ */
+
+public class SharePark {
+    private String place_id;
+    private String place_name;
+
+    public String getPlace_id() {
+        return place_id;
+    }
+
+    public String getPlace_name() {
+        return place_name;
+    }
+
+    public void setPlace_id(String place_id) {
+        this.place_id = place_id;
+    }
+
+    public void setPlace_name(String place_name) {
+        this.place_name = place_name;
+    }
+}
