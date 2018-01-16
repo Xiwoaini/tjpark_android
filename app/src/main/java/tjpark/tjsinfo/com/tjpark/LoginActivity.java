00ac1cab3b78193cb5ed17bca45801537746e1bd
futai <136260130@qq.com>
2018-01-16 09:25:19 +0800

    天津停车需要的基本类

--- app/src/main/java/tjpark/tjsinfo/com/tjpark/LoginActivity.java
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/LoginActivity.java
@@ -1,23 +1,28 @@
 package tjpark.tjsinfo.com.tjpark;
 
-import android.content.Context;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.StrictMode;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.google.gson.JsonObject;
 
-import java.util.HashMap;
-import java.util.Map;
+
+
+import tjpark.tjsinfo.com.tjpark.util.NetConnection;
 
 /**
  * Created by panning on 2018/1/12.
  */
 
-public class LoginActivity  extends AppCompatActivity {
+public class LoginActivity  extends AppCompatActivity  {
 
     //绑定控件属性
     private TextView username;
@@ -28,8 +33,8 @@ public class LoginActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+
         username = (EditText)findViewById(R.id.editText4);
         password = (EditText)findViewById(R.id.editText2);
         regBtn = (Button)findViewById(R.id.button5);
@@ -38,7 +43,10 @@ public class LoginActivity  extends AppCompatActivity {
         loginBtn.setOnClickListener(lb);
 
 
+
     }
+
+
 
 
     //内部类，负责监听登录按钮
@@ -46,6 +54,7 @@ public class LoginActivity  extends AppCompatActivity {
 //监听方法
         @Override
         public void onClick(View view) {
+            new Thread(runnable).start();  //启动子线程
             //用户名或密码为空
             if(username.getText().toString() == null || username.getText().toString() == "" ||
                     password.getText().toString() == null || password.getText().toString() == ""){
@@ -57,29 +66,41 @@ public class LoginActivity  extends AppCompatActivity {
                         .show();
             }
             else{
-                //访问接口
-
-//                Map<String, String> parameters = new HashMap<String, String>();
-//                parameters.put("nameInput", "13920775231");
-//                parameters.put("password", "111111");
-//                parameters.put("registrationId", "1");
-//                String result =sendGet("http://60.29.41.58:3000/tjpark/app/AppWebservice/userLogin", parameters);
-//                System.out.println(result);
-
-
 
             }
 
-
-
         }
-
-
 
     }
 
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Bundle data = msg.getData();
+            String val = data.getString("value");
 
 
+        }
+    };
+
+    //新线程进行网络请求
+    Runnable runnable = new Runnable(){
+        @Override
+        public void run() {
+
+         JsonObject res = null;
+        res =NetConnection.getXpath("/tjpark/app/AppWebservice/userLogin?password=111111&registrationId=1&nameInput=13920775231");
+            System.out.println(res);
+            System.out.println(res.get("result"));
+
+            Message msg = new Message();
+            Bundle data = new Bundle();
+            data.putString("value","请求结果");
+            msg.setData(data);
+            handler.sendMessage(msg);
+        }
+    };
 
 
 
--- app/src/main/java/tjpark/tjsinfo/com/tjpark/util/NetConnection.java
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/util/NetConnection.java
@@ -1,182 +1,105 @@
 package tjpark.tjsinfo.com.tjpark.util;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by panning on 2018/1/15.
  */
 
-public class NetConnection {
+public class NetConnection  {
 
-//    测试函数
-    public static void main(String[] args) {
 
-        Map<String, String> parameters = new HashMap<String, String>();
-        parameters.put("nameInput", "13920775231");
-        parameters.put("password", "111111");
-        parameters.put("registrationId", "1");
-        String result =sendGet("http://60.29.41.58:3000/tjpark/app/AppWebservice/userLogin", parameters);
-        System.out.println(result);
-    }
+
+
 
     /**
-     * 发送GET请求
-     *
-     * @param url
-     *            目的地址
-     * @param parameters
-     *            请求参数，Map类型。
-     * @return 远程响应结果
+     * 发起http请求并获取结果
+     * @param requestUrl 请求地址
      */
-    public static String sendGet(String url, Map<String, String> parameters) {
-        String result="";
-        BufferedReader in = null;// 读取响应输入流
-        StringBuffer sb = new StringBuffer();// 存储参数
-        String params = "";// 编码之后的参数
-        try {
-            // 编码请求参数
-            if(parameters.size()==1){
-                for(String name:parameters.keySet()){
-                    sb.append(name).append("=").append(
-                            java.net.URLEncoder.encode(parameters.get(name),
-                                    "UTF-8"));
+
+        public static JsonObject getXpath(String requestUrl){
+
+        String res="";
+        JsonObject object = null;
+        StringBuffer buffer = new StringBuffer();
+        try{
+            URL url = new URL(TjParkUtils.windowIp+requestUrl);
+            HttpURLConnection urlCon= (HttpURLConnection)url.openConnection();
+
+            if(200==urlCon.getResponseCode()){
+                InputStream is = urlCon.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is,"utf-8");
+                BufferedReader br = new BufferedReader(isr);
+
+                String str = null;
+                while((str = br.readLine())!=null){
+                    buffer.append(str);
                 }
-                params=sb.toString();
-            }else{
-                for (String name : parameters.keySet()) {
-                    sb.append(name).append("=").append(
-                            java.net.URLEncoder.encode(parameters.get(name),
-                                    "UTF-8")).append("&");
-                }
-                String temp_params = sb.toString();
-                params = temp_params.substring(0, temp_params.length() - 1);
+                br.close();
+                isr.close();
+                is.close();
+                res = buffer.toString();
+                JsonParser parse =new JsonParser();
+                object = (JsonObject) parse.parse(res);
             }
-            String full_url = url + "?" + params;
-            System.out.println(full_url);
-            // 创建URL对象
-            java.net.URL connURL = new java.net.URL(full_url);
-            // 打开URL连接
-            java.net.HttpURLConnection httpConn = (java.net.HttpURLConnection) connURL
-                    .openConnection();
-            // 设置通用属性
-            httpConn.setRequestProperty("Accept", "*/*");
-            httpConn.setRequestProperty("Connection", "Keep-Alive");
-            httpConn.setRequestProperty("User-Agent",
-                    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
-            // 建立实际的连接
-            httpConn.connect();
-            // 响应头部获取
-            Map<String, List<String>> headers = httpConn.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : headers.keySet()) {
-                System.out.println(key + "\t：\t" + headers.get(key));
-            }
-            // 定义BufferedReader输入流来读取URL的响应,并设置编码方式
-            in = new BufferedReader(new InputStreamReader(httpConn
-                    .getInputStream(), "UTF-8"));
-            String line;
-            // 读取返回的内容
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
+        }catch(IOException e){
             e.printStackTrace();
-        }finally{
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
         }
-        return result ;
+        return object;
     }
 
 
-
-
-
-
-    /**
-     * 发送POST请求
-     *
-     * @param url
-     *            目的地址
-     * @param parameters
-     *            请求参数，Map类型。
-     * @return 远程响应结果
-     */
-//    public static String sendPost(String url, Map<String, String> parameters) {
-//        String result = "";// 返回的结果
-//        BufferedReader in = null;// 读取响应输入流
-//        PrintWriter out = null;
-//        StringBuffer sb = new StringBuffer();// 处理请求参数
-//        String params = "";// 编码之后的参数
+//    public static JsonObject postDownloadJson(String path,String post){
+//        URL url = null;
 //        try {
-//            // 编码请求参数
-//            if (parameters.size() == 1) {
-//                for (String name : parameters.keySet()) {
-//                    sb.append(name).append("=").append(
-//                            java.net.URLEncoder.encode(parameters.get(name),
-//                                    "UTF-8"));
-//                }
-//                params = sb.toString();
-//            } else {
-//                for (String name : parameters.keySet()) {
-//                    sb.append(name).append("=").append(
-//                            java.net.URLEncoder.encode(parameters.get(name),
-//                                    "UTF-8")).append("&");
-//                }
-//                String temp_params = sb.toString();
-//                params = temp_params.substring(0, temp_params.length() - 1);
-//            }
-//            // 创建URL对象
-//            java.net.URL connURL = new java.net.URL(url);
-//            // 打开URL连接
-//            java.net.HttpURLConnection httpConn = (java.net.HttpURLConnection) connURL
-//                    .openConnection();
-//            // 设置通用属性
-//            httpConn.setRequestProperty("Accept", "*/*");
-//            httpConn.setRequestProperty("Connection", "Keep-Alive");
-//            httpConn.setRequestProperty("User-Agent",
-//                    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
-//            // 设置POST方式
-//            httpConn.setDoInput(true);
-//            httpConn.setDoOutput(true);
-//            // 获取HttpURLConnection对象对应的输出流
-//            out = new PrintWriter(httpConn.getOutputStream());
+//            url = new URL(path);
+//            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//            httpURLConnection.setRequestMethod("POST");// 提交模式
+//            // conn.setConnectTimeout(10000);//连接超时 单位毫秒
+//            // conn.setReadTimeout(2000);//读取超时 单位毫秒
+//            // 发送POST请求必须设置如下两行
+//            httpURLConnection.setDoOutput(true);
+//            httpURLConnection.setDoInput(true);
+//            // 获取URLConnection对象对应的输出流
+//            PrintWriter printWriter = new PrintWriter(httpURLConnection.getOutputStream());
 //            // 发送请求参数
-//            out.write(params);
+//            printWriter.write(post);//post的参数 xx=xx&yy=yy
 //            // flush输出流的缓冲
-//            out.flush();
-//            // 定义BufferedReader输入流来读取URL的响应，设置编码方式
-//            in = new BufferedReader(new InputStreamReader(httpConn
-//                    .getInputStream(), "UTF-8"));
-//            String line;
-//            // 读取返回的内容
-//            while ((line = in.readLine()) != null) {
-//                result += line;
+//            printWriter.flush();
+//            //开始获取数据
+//            BufferedInputStream bis = new            BufferedInputStream(httpURLConnection.getInputStream());
+//            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//            int len;
+//            byte[] arr = new byte[1024];
+//            while((len=bis.read(arr))!= -1){
+//                bos.write(arr,0,len);
+//                bos.flush();
 //            }
+//            bos.close();
+//            JsonParser parse = new JsonParser();
+//            return (JsonObject)parse.parse(bos.toString("utf-8"));
 //        } catch (Exception e) {
 //            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (out != null) {
-//                    out.close();
-//                }
-//                if (in != null) {
-//                    in.close();
-//                }
-//            } catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
 //        }
-//        return result;
+//        return null;
+//    }
+
+//    //测试
+//    public static void main(String args [] ) {
+//        JsonObject res = null;
+//        res = getXpath("/tjpark/app/AppWebservice/userLogin?password=111111&registrationId=1&nameInput=13920775231");
+//        System.out.println(res);
+//        System.out.println(res.get("result"));
+//
 //    }
 }
