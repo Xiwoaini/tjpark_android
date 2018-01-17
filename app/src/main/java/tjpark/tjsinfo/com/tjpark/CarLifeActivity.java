42c9676512e65e667b029866d90a34ff809a28b5
futai <136260130@qq.com>
2018-01-17 10:00:45 +0800

    天津停车需要的基本类

--- app/src/main/java/tjpark/tjsinfo/com/tjpark/CarLifeActivity.java
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/CarLifeActivity.java
@@ -1,17 +1,100 @@
 package tjpark.tjsinfo.com.tjpark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by panning on 2018/1/12.
  */
 
+//车生活的12个按钮
 public class CarLifeActivity  extends AppCompatActivity {
+
+    //绑定控件属性
+      private Button carLifeBtn1;
+//    private Button carLifeBtn2;
+//    private Button carLifeBtn3;
+//    private Button carLifeBtn4;
+//    private Button carLifeBtn5;
+//    private Button carLifeBtn6;
+//    private Button carLifeBtn7;
+//    private Button carLifeBtn8;
+//    private Button carLifeBtn9;
+//    private Button carLifeBtn10;
+//    private Button carLifeBtn11;
+//    private Button carLifeBtn12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_carlife);
+        carLifeBtn1 = (Button) findViewById(R.id.button1);
+//
+//
+//
+//        //对登录按钮进行监听
+        CarLifeBtn clb = new CarLifeBtn();
+        carLifeBtn1.setOnClickListener(clb);
     }
+
+    //内部类，负责监听12个按钮
+    class CarLifeBtn implements View.OnClickListener{
+    //添加监听按钮
+        @Override
+        public void onClick(View view) {
+            if (carLifeBtn1.getText().toString().trim().equals("注册")){
+                Intent intent = new Intent();
+
+          //(当前Activity，目标Activity)
+                intent.setClass(CarLifeActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else if (carLifeBtn1.getText().toString().trim().equals("注册")) {
+
+            }
+            else{
+                Intent intent = new Intent();
+                //(当前Activity，目标Activity)
+                intent.setClass(CarLifeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+
+
+
+        }
+    }
+
+
 }
--- app/src/main/java/tjpark/tjsinfo/com/tjpark/LoginActivity.java
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/LoginActivity.java
@@ -1,14 +1,18 @@
 package tjpark.tjsinfo.com.tjpark;
 
 
-import android.os.Build;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.StrictMode;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,7 +26,8 @@ import tjpark.tjsinfo.com.tjpark.util.NetConnection;
  * Created by panning on 2018/1/12.
  */
 
-public class LoginActivity  extends AppCompatActivity  {
+//登录管理类
+public class LoginActivity  extends AppCompatActivity {
 
     //绑定控件属性
     private TextView username;
@@ -35,51 +40,49 @@ public class LoginActivity  extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //获取对应控件
         username = (EditText)findViewById(R.id.editText4);
         password = (EditText)findViewById(R.id.editText2);
         regBtn = (Button)findViewById(R.id.button5);
         loginBtn = (Button)findViewById(R.id.button6);
+        //对登录按钮进行监听
         LoginBtn lb = new LoginBtn();
         loginBtn.setOnClickListener(lb);
 
-
-
     }
 
 
-
-
-    //内部类，负责监听登录按钮
-    class LoginBtn implements View.OnClickListener{
-//监听方法
-        @Override
-        public void onClick(View view) {
-            new Thread(runnable).start();  //启动子线程
-            //用户名或密码为空
-            if(username.getText().toString() == null || username.getText().toString() == "" ||
-                    password.getText().toString() == null || password.getText().toString() == ""){
-
-                new AlertDialog.Builder(LoginActivity.this)
-                        .setTitle("注意")
-                        .setMessage("请输入用户名或密码!")
-                        .setPositiveButton("确定", null)
-                        .show();
-            }
-            else{
-
-            }
-
-        }
-
-    }
-
+    //处理登录结果
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             Bundle data = msg.getData();
             String val = data.getString("value");
+            if (val.equals("\"1\"")){
+                //还没有被注册
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("提示")
+                        .setMessage("此用户还没有呗注册!")
+                        .setPositiveButton("确定", null)
+                        .show();
+            }
+            else if (val.equals("\"2\"")){
+                //用户或密码错误
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("提示")
+                        .setMessage("用户名或密码不正确!")
+                        .setPositiveButton("确定", null)
+                        .show();
+            }
+            else{
+                //成功登陆，进行跳转
+//                Intent intent = new Intent();
+//          //(当前Activity，目标Activity)
+//                intent.setClass(LoginActivity.this, PersonActivity.class);
+//                startActivity(intent);
 
+            }
 
         }
     };
@@ -89,19 +92,48 @@ public class LoginActivity  extends AppCompatActivity  {
         @Override
         public void run() {
 
-         JsonObject res = null;
-        res =NetConnection.getXpath("/tjpark/app/AppWebservice/userLogin?password=111111&registrationId=1&nameInput=13920775231");
-            System.out.println(res);
-            System.out.println(res.get("result"));
+            JsonObject res = null;
+//            /tjpark/app/AppWebservice/userLogin?password=111111&registrationId=1&nameInput=13920775231
+            String strUrl="/tjpark/app/AppWebservice/userLogin?nameInput="+username.getText().toString()+"&password="
+                    +password.getText().toString()+"&registrationId=1";
+            res =NetConnection.getXpath(strUrl);
+            //全部返回的字符串内容
+//            System.out.println(res);
+//              System.out.println(res.get("result"));
+            String result = res.get("result").toString();
 
             Message msg = new Message();
             Bundle data = new Bundle();
-            data.putString("value","请求结果");
+            data.putString("value",result);
             msg.setData(data);
             handler.sendMessage(msg);
         }
     };
 
+    //内部类，负责监听登录按钮
+    class LoginBtn implements View.OnClickListener{
+//监听方法
+        @Override
+        public void onClick(View view) {
+
+
+            //用户名或密码为空
+            if(TextUtils.isEmpty(username.getText().toString().trim()) || TextUtils.isEmpty(password.getText().toString().trim())){
+
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("注意")
+                        .setMessage("请输入用户名或密码!")
+                        .setPositiveButton("确定", null)
+                        .show();
+            }
+            else{
+                //安卓访问http需要在子线程中进行
+                new Thread(runnable).start();
+            }
+
+        }
+
+    }
 
 
 
