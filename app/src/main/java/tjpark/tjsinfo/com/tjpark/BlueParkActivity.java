00d87d971911be4c203951b797eaff7ecb78eb7e
futai <136260130@qq.com>
2018-01-18 13:51:33 +0800

    天津停车需要的基本类

---
+++ app/src/main/java/tjpark/tjsinfo/com/tjpark/BlueParkActivity.java
@@ -0,0 +1,17 @@
+package tjpark.tjsinfo.com.tjpark;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by panning on 2018/1/12.
+ */
+
+public class BlueParkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcar);
+    }
+}
